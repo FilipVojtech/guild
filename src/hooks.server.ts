a0ab6e2811c40ddb6d960ee3a065f97e8f64dcd4
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2023-04-14 21:30:03 +0100

    Implement Jose

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,36 +1,49 @@
 import type { Handle } from '@sveltejs/kit';
-import * as jose from 'jose';
-import { JWT_SECRET } from '$env/static/private';
 import orm from '$lib/server/database';
 import User from '$lib/server/entities/User';
-import { createSecretKey } from 'crypto';
+import { verifyToken } from '$lib/server/JWTToken';
+import { refreshToken } from './lib/server/JWTToken';
+import type { JWTPayload } from 'jose';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const { cookies } = event;
-	const token = cookies.get('token');
+	const cookieToken = cookies.get('token');
+
+	if (!cookieToken) {
+		return resolve(event);
+	}
+
+	let payload: JWTPayload | null = await verifyToken(cookieToken);
+	if (!payload) {
+		let refreshedToken = await refreshToken(cookieToken);
+		if (!refreshedToken) {
+			return resolve(event);
+		} else {
+			payload = await verifyToken(refreshedToken);
+			if (!payload) {
+				return resolve(event);
+			}
+		}
+	}
+
+	let user;
 	const em = orm.em.fork();
 
-	if (token) {
-		const secret = createSecretKey(JWT_SECRET, 'utf-8');
-		const { payload, protectedHeader } = await jose.jwtVerify(token, secret);
-
-		let user;
-		try {
-			user = await em.findOne(User, { id: payload.id! });
-		} catch (e) {
-			console.error(e);
-			return resolve(event);
-		}
-
-		if (!user) {
-			throw new Error('User not found');
-		}
-
-		event.locals.user = {
-			id: user.id,
-			displayName: user.displayName,
-		} as App.SessionUser;
+	try {
+		user = await em.findOne(User, { id: payload.id! });
+	} catch (e) {
+		console.error(e);
+		return resolve(event);
 	}
+
+	if (!user) {
+		throw new Error('User not found');
+	}
+
+	event.locals.user = {
+		id: user.id,
+		displayName: user.displayName,
+	} as App.SessionUser;
 
 	return resolve(event);
 };
